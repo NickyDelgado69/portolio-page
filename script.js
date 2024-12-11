d57f38a9ce9097d1cd22662e0ba3317d04b8e487
Nicky Delgado <gerbf9@bfh.ch>
2024-12-11 18:29:54 +0100

    spotify api

--- script.js
+++ script.js
@@ -39,3 +39,38 @@ emailjs
   console.error("EmailJS error:", error);
 });
 });
+
+async function getAccessToken() {
+  const clientId = ''; // Ersetze mit deinem Client ID
+  const clientSecret = ''; // Ersetze mit deinem Client Secret
+
+  const tokenUrl = 'https://accounts.spotify.com/api/token';
+
+  const headers = new Headers();
+  headers.append('Authorization', 'Basic ' + btoa(clientId + ':' + clientSecret));
+  headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+  const body = new URLSearchParams();
+  body.append('grant_type', 'client_credentials');
+
+  try {
+      const response = await fetch(tokenUrl, {
+          method: 'POST',
+          headers: headers,
+          body: body
+      });
+
+      if (!response.ok) throw new Error('Token Request Failed');
+
+      const data = await response.json();
+      const accessToken = data.access_token;
+
+      console.log('Access Token:', accessToken);
+      return accessToken;
+
+  } catch (error) {
+      console.error('Error fetching the access token:', error);
+  }
+}
+
+getAccessToken();
